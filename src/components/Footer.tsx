9fbe13ea4f8577516dc094de859c9e486031a329
muhamad-naufal <muhamadnaufal594@gmail.com>
2024-11-15 09:56:50 +0700

    footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,9 +15,9 @@ const Footer = () => {
             quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
           </p>
         </div>
-        <div>
+        <div className="w-full md:w-1/5 mb-10 md:mb-0">
           <h3 className="text-white text-lg font-bold mb-4">Company</h3>
-          <ul>
+          <ul className="flex flex-col gap-2 text-gray-400">
             <a href="#home" className="hover:text-white">
               Home
             </a>
@@ -28,13 +28,38 @@ const Footer = () => {
               Project
             </a>
             <a href="#testi" className="hover:text-white">
-              Home
+              Testimonial
+            </a>
+            <a href="#" className="hover:text-white">
+              Privacy Policy
             </a>
           </ul>
         </div>
-        <div></div>
+        <div className="w-full md:w-1/3">
+          <h3 className="text-white text-lg font-bold mb-4">Subscribe</h3>
+          <p className="text-gray-400 mb-4 max-w-80">
+            Subscribe to our newsletter to get the latest news and promotions.
+          </p>
+          <div className="flex gap-2">
+            <input
+              type="text"
+              placeholder="Enter your email"
+              className="p-2 rounded bg-gray-800 text-gray-400 border border-gray-700 focus:outline-none w-full md:w-auto"
+            />
+            <button className="bg-red-500 text-white px-4 py-2">
+              Subscribe
+            </button>
+          </div>
+        </div>
       </div>
-      <div></div>
+      <div>
+        <p className="text-center text-gray-400 mt-10 mb-5">
+          © 2021 All Rights Reserved. Designed by{" "}
+          <a href="#" className="text-red-500" target="_blank" rel="noreferrer">
+            Digital Meta Generasi
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
